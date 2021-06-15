efae151359e06ac2df9dd8d21779e1c08fe83c2c
Kevin Niu <kevin.h.niu@gmail.com>
2021-06-15 14:50:26 -0500

    Beginning of Main Method Interface(Input and While Loop)
    Built the first while loop, arraylist, and input

--- Activity5/Activity5/Program.cs
+++ Activity5/Activity5/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections;
 
 namespace Activity5
 {
+
     class Program
     {
+        public static ArrayList listOfNumbers = new ArrayList();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter number(Enter -999 to quit):");
+            int integerInput = Convert.ToInt32(Console.ReadLine());
+            while(integerInput != -999)
+            {
+                listOfNumbers.Add(integerInput);
+                Console.WriteLine("Enter number(Enter -999 to quit):");
+                integerInput = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
