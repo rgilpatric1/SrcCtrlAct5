73fd3d84901ee2415bbfad5a2f0c0d8f9bcdca9c
Sumanth Kota <sumanthkota684@gmail.com>
2021-06-16 09:44:22 -0500

    Display Even numbers and numbers divisible by three.

--- Activity5/Activity5/Program.cs
+++ Activity5/Activity5/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-
+using System.Collections;
 namespace Activity5
 {
     class Program
     {
+      
+   static ArrayList listOfNumbers = new ArrayList();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter number(Enter -999 to quit):");
+            int integerInput = Convert.ToInt32(Console.ReadLine());
+            while (integerInput != -999)
+            {
+                listOfNumbers.Add(integerInput);
+                Console.WriteLine("Enter number(Enter -999 to quit):");
+                integerInput = Convert.ToInt32(Console.ReadLine());
+            }
+        }//end of main
+        static void displayEvenIntegers()
+        {
+            foreach (int element in listOfNumbers)
+            {
+                if (element % 2 == 0)
+                {
+                    Console.WriteLine(element);
+                }
+            }
         }
+        static void displayNumbersDivisibleByThree()
+        {
+            foreach (int element in listOfNumbers)
+            {
+                if (element % 3 == 0)
+                {
+                    Console.WriteLine(element);
+                }
+
+            }
+
+        }
+    }
     }
 }
